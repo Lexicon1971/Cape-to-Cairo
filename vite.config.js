8eae0cf9aa621e509cdd92182c425a8422605bec
Lexicon1971 <159695754+Lexicon1971@users.noreply.github.com>
2026-07-10 17:59:59 +0200

    Configure base path and server settings in vite.config.js
    Set base path for deployment and adjusted server configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Cape-to-Cairo/', // Matches your GitHub repository name exactly
   server: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
+    port: 3000
+  }
 });
